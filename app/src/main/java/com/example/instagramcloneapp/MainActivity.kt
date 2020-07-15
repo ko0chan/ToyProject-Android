bbfc33715d9b190b6bf95f0647e3b3f4d32870ef
YoungChan <kychan1320@gmail.com>
2020-07-15 21:15:12 +0900

    onNavigationItemSelectedListener

--- app/src/main/java/com/example/instagramcloneapp/MainActivity.kt
+++ app/src/main/java/com/example/instagramcloneapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.instagramcloneapp
 
 import android.os.Bundle
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -10,8 +11,41 @@ import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var textView: TextView
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                textView.text = "Home"
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_search -> {
+                textView.text = "Search"
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_add_post -> {
+                textView.text = "Add Post"
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notifications -> {
+                textView.text = "Notifications"
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+                textView.text = "Profile"
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener { onNavigationItemSelectedListener }
     }
 }
